ac98e1768d47da872c23e18032db6e294dcb616b
flareguner <flareguner@gmail.com>
2014-01-23 14:30:25 +0400

    remove clear button

--- src/texttoolbar.h
+++ src/texttoolbar.h
@@ -10,12 +10,10 @@ class TextToolbar : public QToolBar
 public:
     explicit TextToolbar(QWidget *parent = 0);
 signals:
-    void requestClear();
     void requestCopy();
     void requestPronounce();
 private:
     void changeEvent(QEvent *e);
-    QAction *action_clear;
     QAction *action_copy;
     QAction *action_pronounce;
 };
