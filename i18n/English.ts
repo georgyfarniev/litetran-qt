9bbbee9bda773b250c935b07d239549a7c2aa3e0
flareguner <flareguner@gmail.com>
2014-04-12 20:23:13 +0400

    fix english translation

--- i18n/English.ts
+++ i18n/English.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.0" language="en">
 <context>
     <name>Languages</name>
     <message>
         <location filename="../src/languages.cpp" line="101"/>
         <source>Set\Unset All</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/languages.cpp" line="102"/>
         <source>Languages</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/languages.cpp" line="103"/>
         <source>At least two languages should be selected to work correctly.</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -25,53 +25,53 @@
         <location filename="../src/mainwindow.cpp" line="225"/>
         <location filename="../src/mainwindow.cpp" line="233"/>
         <source>Swap languages</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="226"/>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="227"/>
         <source>Languages</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="228"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="229"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="230"/>
         <source>Open LiteTran menu</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="231"/>
         <location filename="../src/mainwindow.cpp" line="232"/>
         <source>Translate</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="234"/>
         <source>Source language</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="235"/>
         <source>Result language</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="237"/>
         <source>About LiteTran</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="238"/>
@@ -80,12 +80,12 @@
 LiteTran is licensed under GPL3
 Homepage: https://github.com/flareguner/litetran
 Version: </source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="247"/>
         <source>Enter text to translate here...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -93,42 +93,42 @@ Version: </source>
     <message>
         <location filename="../src/settings.cpp" line="121"/>
         <source>Translate and reverse key sequences shouldn&apos;t be overlapped!</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="122"/>
         <source>Invalid shortcuts</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="123"/>
         <source>Show icon in system tray</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="124"/>
         <source>Translate</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="125"/>
         <source>Reverse translate</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="126"/>
         <source>Show dictionary results</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="127"/>
         <source>Application language</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="128"/>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -136,12 +136,12 @@ Version: </source>
     <message>
         <location filename="../src/texttoolbar.cpp" line="37"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/texttoolbar.cpp" line="38"/>
         <source>Pronounce</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
