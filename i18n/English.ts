9cbd175b3c6da920910d31cc894a716f80272f6a
flareguner <flareguner@gmail.com>
2014-04-13 19:59:40 +0400

    update translations

--- i18n/English.ts
+++ i18n/English.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en">
+<TS version="2.1" language="en">
 <context>
     <name>Languages</name>
     <message>
@@ -22,68 +22,73 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/mainwindow.cpp" line="225"/>
-        <location filename="../src/mainwindow.cpp" line="233"/>
+        <location filename="../src/mainwindow.cpp" line="226"/>
+        <location filename="../src/mainwindow.cpp" line="234"/>
         <source>Swap languages</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="226"/>
+        <location filename="../src/mainwindow.cpp" line="227"/>
         <source>Configure</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="227"/>
+        <location filename="../src/mainwindow.cpp" line="228"/>
         <source>Languages</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="228"/>
+        <location filename="../src/mainwindow.cpp" line="229"/>
         <source>About</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="229"/>
+        <location filename="../src/mainwindow.cpp" line="230"/>
         <source>Exit</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="230"/>
+        <location filename="../src/mainwindow.cpp" line="231"/>
         <source>Open LiteTran menu</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="231"/>
         <location filename="../src/mainwindow.cpp" line="232"/>
+        <location filename="../src/mainwindow.cpp" line="233"/>
         <source>Translate</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="234"/>
+        <location filename="../src/mainwindow.cpp" line="235"/>
         <source>Source language</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="235"/>
+        <location filename="../src/mainwindow.cpp" line="236"/>
         <source>Result language</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="237"/>
+        <location filename="../src/mainwindow.cpp" line="238"/>
         <source>About LiteTran</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="238"/>
+        <location filename="../src/mainwindow.cpp" line="239"/>
         <source>LiteTran is a lightweight text translation program.Just select some text, press hotkey and get translation in small popup!
 
 LiteTran is licensed under GPL3
 Homepage: https://github.com/flareguner/litetran
-Version: </source>
+
+
+Authors:
+flareguner - author &lt;flarguner@gmail.com&gt;
+Yurij Mikhalevich - Mac OS port and testing &lt;0@39.yt&gt;
+Uri Herrera, Nitrux S.A. - Icon and logo &lt;contact@nitrux.in&gt;</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="247"/>
+        <location filename="../src/mainwindow.cpp" line="248"/>
         <source>Enter text to translate here...</source>
         <translation></translation>
     </message>
@@ -91,42 +96,42 @@ Version: </source>
 <context>
     <name>Settings</name>
     <message>
-        <location filename="../src/settings.cpp" line="121"/>
+        <location filename="../src/settings.cpp" line="120"/>
         <source>Translate and reverse key sequences shouldn&apos;t be overlapped!</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/settings.cpp" line="122"/>
+        <location filename="../src/settings.cpp" line="121"/>
         <source>Invalid shortcuts</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/settings.cpp" line="123"/>
+        <location filename="../src/settings.cpp" line="122"/>
         <source>Show icon in system tray</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/settings.cpp" line="124"/>
+        <location filename="../src/settings.cpp" line="123"/>
         <source>Translate</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/settings.cpp" line="125"/>
+        <location filename="../src/settings.cpp" line="124"/>
         <source>Reverse translate</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/settings.cpp" line="126"/>
+        <location filename="../src/settings.cpp" line="125"/>
         <source>Show dictionary results</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/settings.cpp" line="127"/>
+        <location filename="../src/settings.cpp" line="126"/>
         <source>Application language</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/settings.cpp" line="128"/>
+        <location filename="../src/settings.cpp" line="127"/>
         <source>Configure</source>
         <translation></translation>
     </message>
--- i18n/Russian.ts
+++ i18n/Russian.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>Languages</name>
     <message>
         <location filename="../src/languages.cpp" line="101"/>
         <source>Set\Unset All</source>
-        <translation>Выбрать\Убрать Всё</translation>
+        <translation>Выбрать\Убрать всё</translation>
     </message>
     <message>
         <location filename="../src/languages.cpp" line="102"/>
@@ -16,187 +16,137 @@
     <message>
         <location filename="../src/languages.cpp" line="103"/>
         <source>At least two languages should be selected to work correctly.</source>
-        <translation>Для корректной работы программы необходимо как минимум 2 языка.</translation>
+        <translation>Необходимо как минимум два языка для корректной работы программы.</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/mainwindow.cpp" line="231"/>
-        <location filename="../src/mainwindow.cpp" line="232"/>
-        <source>Translate</source>
-        <translation>Перевести</translation>
-    </message>
-    <message>
-        <source>Settings</source>
-        <translation>Настройки</translation>
-    </message>
-    <message>
-        <location filename="../src/mainwindow.cpp" line="225"/>
-        <location filename="../src/mainwindow.cpp" line="233"/>
-        <source>Swap languages</source>
-        <translation>Поменять языки местами</translation>
-    </message>
-    <message>
         <location filename="../src/mainwindow.cpp" line="226"/>
-        <source>Configure</source>
-        <translation>Настройки</translation>
-    </message>
-    <message>
-        <source>Detect language</source>
-        <translation>Определить язык</translation>
+        <location filename="../src/mainwindow.cpp" line="234"/>
+        <source>Swap languages</source>
+        <translation>Поменять языки</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="227"/>
+        <source>Configure</source>
+        <translation>Настроить</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="228"/>
         <source>Languages</source>
         <translation>Языки</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="228"/>
+        <location filename="../src/mainwindow.cpp" line="229"/>
         <source>About</source>
         <translation>О программе</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="229"/>
+        <location filename="../src/mainwindow.cpp" line="230"/>
         <source>Exit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="230"/>
+        <location filename="../src/mainwindow.cpp" line="231"/>
         <source>Open LiteTran menu</source>
         <translation>Открыть меню LiteTran</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="234"/>
+        <location filename="../src/mainwindow.cpp" line="232"/>
+        <location filename="../src/mainwindow.cpp" line="233"/>
+        <source>Translate</source>
+        <translation>Перевод</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="235"/>
         <source>Source language</source>
         <translation>Исходный язык</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="235"/>
+        <location filename="../src/mainwindow.cpp" line="236"/>
         <source>Result language</source>
         <translation>Язык перевода</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="238"/>
-        <source>LiteTran is a lightweight text translation program.Just select some text, press hotkey and get translation in small popup!
-
-LiteTran is licensed under GPL3
-Homepage: https://github.com/flareguner/litetran
-Version: </source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>LiteTran is a lightweight text translation program.Just select some text, press hotkey and get translation in small popup!
-
-LiteTran is licensed under GPL3
-Homepage: https://github.com/flareguner/litetran
-
-Authors:
-flarguner@gmail.com (flareguner) - author
-0@39.yt (Yurij Mikhalevich) - Mac OS port and testing</source>
-        <translation>LiteTran - это простая программа для перевода текста. Просто выделите текст, нажмите комбинацию клавиш и получите всплывающую подсказку с переводом
-!
-LIteTran лицензирован под GPL3
-Домашняя страница: https://github.com/flareguner/litetran
-
-Авторы:
-flarguner@gmail.com (flareguner) - автор
-0@39.yt (Yurij Mikhalevich) - портирование и тестирование в Mac OS</translation>
-    </message>
-    <message>
-        <location filename="../src/mainwindow.cpp" line="247"/>
-        <source>Enter text to translate here...</source>
-        <translation>Введите текст для перевода тут...</translation>
-    </message>
-    <message>
-        <source>Menu</source>
-        <translation>Меню</translation>
-    </message>
-    <message>
-        <source>menu</source>
-        <translation>меню</translation>
-    </message>
-    <message>
-        <location filename="../src/mainwindow.cpp" line="237"/>
         <source>About LiteTran</source>
         <translation>О LiteTran</translation>
     </message>
     <message>
+        <location filename="../src/mainwindow.cpp" line="239"/>
         <source>LiteTran is a lightweight text translation program.Just select some text, press hotkey and get translation in small popup!
 
 LiteTran is licensed under GPL3
-Homepage: https://github.com/flareguner/litetran</source>
-        <translation>LiteTran представляет собой легкую программу для перевода текстов. Просто выделите текст, нажмите комбинацию клавиш и получите перевод во всплывающей подсказке!
+Homepage: https://github.com/flareguner/litetran
+
+
+Authors:
+flareguner - author &lt;flarguner@gmail.com&gt;
+Yurij Mikhalevich - Mac OS port and testing &lt;0@39.yt&gt;
+Uri Herrera, Nitrux S.A. - Icon and logo &lt;contact@nitrux.in&gt;</source>
+        <translation>LiteTran - это небольшая программа для перевода текста. Просто выделите текст, нажмите комбинацию клавиш, и получите перевод в небольшом всплывающем окне!
 
 LiteTran лицензирован под GPL3
-Домашняя страница: https://github.com/flareguner/litetran</translation>
+Домашняя страница: https://github.com/flareguner/litetran
+
+
+Авторы:
+flareguner - автор &lt;flarguner@gmail.com&gt;
+Yurij Mikhalevich - Портирование и тестирование Mac OS &lt;0@39.yt&gt;
+Uri Herrera, Nitrux S.A. - автор иконки и логотипа &lt;contact@nitrux.in&gt;</translation>
     </message>
     <message>
-        <source>Options</source>
-        <translation>Опции</translation>
+        <location filename="../src/mainwindow.cpp" line="248"/>
+        <source>Enter text to translate here...</source>
+        <translation>Введите текст для перевода...</translation>
     </message>
 </context>
 <context>
     <name>Settings</name>
     <message>
-        <source>Show icon in tray</source>
-        <translation>Показывать значок в трее</translation>
-    </message>
-    <message>
-        <location filename="../src/settings.cpp" line="123"/>
-        <source>Show icon in system tray</source>
-        <translation>Показывать значок в трее</translation>
-    </message>
-    <message>
-        <source>Popup translate by shortcut</source>
-        <translation>Перевод по нажатию</translation>
+        <location filename="../src/settings.cpp" line="120"/>
+        <source>Translate and reverse key sequences shouldn&apos;t be overlapped!</source>
+        <translation>Сочетания клавиш для перевода и обратного перевода должны быть разными!</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="121"/>
-        <source>Translate and reverse key sequences shouldn&apos;t be overlapped!</source>
-        <translation>Комбинации клавиш для перевода и обратного перевода не должны быть одинаковыми!</translation>
+        <source>Invalid shortcuts</source>
+        <translation>Некорректное сочетание клавиш</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="122"/>
-        <source>Invalid shortcuts</source>
-        <translation>Неправильное сочетание клавиш</translation>
+        <source>Show icon in system tray</source>
+        <translation>Показывать иконку в трее</translation>
+    </message>
+    <message>
+        <location filename="../src/settings.cpp" line="123"/>
+        <source>Translate</source>
+        <translation>Перевод</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="124"/>
-        <source>Translate</source>
-        <translation>Перевести</translation>
-    </message>
-    <message>
-        <location filename="../src/settings.cpp" line="125"/>
         <source>Reverse translate</source>
         <translation>Обратный перевод</translation>
     </message>
     <message>
-        <location filename="../src/settings.cpp" line="126"/>
+        <location filename="../src/settings.cpp" line="125"/>
         <source>Show dictionary results</source>
         <translation>Показывать варианты перевода</translation>
     </message>
     <message>
-        <location filename="../src/settings.cpp" line="127"/>
+        <location filename="../src/settings.cpp" line="126"/>
         <source>Application language</source>
-        <translation>Язык интерфейса</translation>
+        <translation>Язык приложения</translation>
     </message>
     <message>
-        <location filename="../src/settings.cpp" line="128"/>
+        <location filename="../src/settings.cpp" line="127"/>
         <source>Configure</source>
-        <translation>Настройки</translation>
-    </message>
-    <message>
-        <source>GUI Language</source>
-        <translation>Язык интерфейса</translation>
+        <translation>Настроить</translation>
     </message>
 </context>
 <context>
     <name>TextToolbar</name>
-    <message>
-        <source>Clear</source>
-        <translation>Очистить</translation>
-    </message>
     <message>
         <location filename="../src/texttoolbar.cpp" line="37"/>
         <source>Copy</source>
@@ -205,10 +155,6 @@ LiteTran лицензирован под GPL3
     <message>
         <location filename="../src/texttoolbar.cpp" line="38"/>
         <source>Pronounce</source>
-        <translation>Произнести</translation>
-    </message>
-    <message>
-        <source>Play</source>
         <translation>Произнести</translation>
     </message>
 </context>
