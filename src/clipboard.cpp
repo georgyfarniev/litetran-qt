57282bad708c13dbc8cbe2942d9a858049fd94e7
flareguner <flareguner@gmail.com>
2014-09-07 20:19:08 +0400

    get selection properly in win32

--- src/clipboard.cpp
+++ src/clipboard.cpp
@@ -20,37 +20,17 @@ QString Clipboard::selectedText() const {
 #if defined(APP_WM_X11)
     text = clipboard->text(QClipboard::Selection);
 #elif defined(APP_WM_WINDOWS)
-    Sleep(200);
-    // Send Ctrl+C to current window
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.wScan = 0;
-    ip.ki.time = 0;
-    ip.ki.dwExtraInfo = 0;
+    HWND hwnd = GetActiveWindow();
+    if (IsWindow(hwnd)) {
+//        SendMessage(hwnd, WM_KEYDOWN, VK_CONTROL, 0);
+//        SendMessage(hwnd, WM_KEYDOWN, 'C', 0);
+//        SendMessage(hwnd, WM_KEYUP, 'C', 0);
+//        SendMessage(hwnd, WM_KEYUP, VK_CONTROL, 0);
+        SendMessage(hwnd, WM_COPY, 0, 0);
 
-    // Press the "Ctrl" key
-    ip.ki.wVk = VK_CONTROL;
-    ip.ki.dwFlags = 0; // 0 for key press
-    SendInput(1, &ip, sizeof(INPUT));
-
-    // Press the "V" key
-    ip.ki.wVk = 'C';
-    ip.ki.dwFlags = 0; // 0 for key press
-    SendInput(1, &ip, sizeof(INPUT));
-
-    // Release the "V" key
-    ip.ki.wVk = 'C';
-    ip.ki.dwFlags = KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
-
-    // Release the "Ctrl" keysd
-    ip.ki.wVk = VK_CONTROL;
-    ip.ki.dwFlags = KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
-
-    Sleep(200);
-
-    text = clipboard->text(QClipboard::Clipboard);
+        Sleep(200);
+        text = clipboard->text(QClipboard::Clipboard);
+    }
 #elif defined(APP_WM_COCOCA)
     QFile file(APP_OSX_TEXTFILE);
     if(!file.open(QFile::ReadOnly | QFile::Text))
